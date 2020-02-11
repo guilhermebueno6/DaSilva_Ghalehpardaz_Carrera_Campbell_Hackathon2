a93f4fdf41c42590fb12caeedd92f4a494cfec4e
Walter Sebastian Carrera <cwalter1994@gmail.com>
2020-02-10 23:45:04 -0500

    initial sass styling

---
+++ src/components/LocationBanner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="location-banner">
+    <div class="banner">
+      <div class="banner-content" :style="{ backgroundImage: `url('${location.image}')` }">
+        <h1 class="destination">{{ location.destination }}</h1>
+        <p class="description">{{ location.description }}</p>
+        <ul class="activities">
+          <li>
+            <a :href="location.url1">
+              {{ location.icon1 }}
+              {{ location.activity1 }}
+            </a>
+          </li>
+          <li>
+            <a :href="location.url2">
+              {{ location.icon2 }}
+              {{ location.activity2 }}
+            </a>
+          </li>
+          <li>
+            <a :href="location.url3">
+              {{ location.icon3 }}
+              {{ location.activity3 }}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: [
+    'location'
+  ]
+}
+</script>
+
---
+++ src/components/LocationList.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="locations">
+    <h2>Places to Visit</h2>
+    <div v-for="location in locations" :location="location" :key="location.id">
+      <router-link :to="'/location/' + location.id">
+        <LocationBanner :location="location" />
+      </router-link>
+    </div>
+  </section>
+</template>
+<script>
+import LocationBanner from "@/components/LocationBanner";
+export default {
+  name: 'LocationList',
+  components : {
+    LocationBanner
+  }
+}
+</script>
+
